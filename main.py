2be64ab2cb6cf8d2f04d97ff343a27f05bfd2121
Abby is good! <saanvip904@gmail.com>
2026-04-30 10:18:40 -0700

    pseudo code
    I did the pseudo code that so then i have an Idea of what the actual code is going to look like in plain english

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+survival caluclator 
+this gane will calculate if you are paying for your phone plan,
+paying for monthly groceries, paying rent,and then it will calculate the total payment 
+for the month.
+"""
+
+# Import tinker for GUI making 
+
+# Set up the root window 
+
+# function for doing the calulation 
+
+# get monthly grocery pay
+
+# get phone plan pay
+
+# get calculate button
+
+# make radio button 
+
+# make a check button 
+
+# create and place GUI widgets on a grid 
+
+# monthly payment result label 
+
+# event loop 
